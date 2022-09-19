3fa7eb8546d5938254052e13b779bc1cb0fdc9a1
Smileher <vchenhe@live.com>
2022-09-19 22:27:24 +0800

    Init

---
+++ KeyBoardTestUtility/KeyboardHock.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+
+namespace KeyBoardTestUtility
+{
+    /// <summary>
+    /// 屏蔽所有的系统按键
+    /// </summary>
+    class KeyboardHock
+    {
+        private struct KBDLLHOOKSTRUCT
+        {
+            public int vkCode;
+            int scanCode;
+            public int flags;
+            int time;
+            int dwExtraInfo;
+        }
+        public KeyboardHock()
+        {
+            IntPtr hModule = GetModuleHandle(IntPtr.Zero);
+            hookProc = new LowLevelKeyboardProcDelegate(LowLevelKeyboardProc);
+            hHook = SetWindowsHookEx(WH_KEYBOARD_LL, hookProc, hModule, 0);
+            if (hHook == IntPtr.Zero)
+            {
+                Console.WriteLine("Failed to set hook, error = " + Marshal.GetLastWin32Error());
+            }
+        }
+        private int LowLevelKeyboardProc(int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam)
+        {
+            if (nCode >= 0)
+            {
+                MainWindow.GetInstance().KeyDown(lParam.vkCode.ToString());
+                return 1;
+            }
+            else
+            {
+                return CallNextHookEx(0, nCode, wParam, ref lParam);
+            }
+            //if (nCode >= 0)
+            //    switch (wParam)
+            //    {
+            //        //屏蔽下面的快捷键
+            //        case 256: // WM_KEYDOWN
+            //        case 257: // WM_KEYUP
+            //        case 260: // WM_SYSKEYDOWN
+            //        case 261: // M_SYSKEYUP
+            //            if (
+            //                (lParam.vkCode == 0x09 && lParam.flags == 32) || // Alt+Tab
+            //                (lParam.vkCode == 0x1b && lParam.flags == 32) || // Alt+Esc
+            //                (lParam.vkCode == 0x20 && lParam.flags == 32) || // Alt+Space
+            //                (lParam.vkCode == 0x73 && lParam.flags == 32) || // Alt+F4
+            //                (lParam.vkCode == 0x1b && lParam.flags == 0) || // Ctrl+Esc
+            //                (lParam.vkCode == 0x5b && lParam.flags == 1) || // Left Windows Key 
+            //                (lParam.vkCode == 0x5c && lParam.flags == 1))    // Right Windows Key 
+            //            {
+            //                return 1;
+            //            }
+            //            break;
+            //    }
+            //return CallNextHookEx(0, nCode, wParam, ref lParam);
+        }
+
+        private delegate int LowLevelKeyboardProcDelegate(int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProcDelegate lpfn, IntPtr hMod, int dwThreadId);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnhookWindowsHookEx(IntPtr hHook);
+
+        [DllImport("user32.dll")]
+        private static extern int CallNextHookEx(int hHook, int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetModuleHandle(IntPtr path);
+
+        private IntPtr hHook;
+        LowLevelKeyboardProcDelegate hookProc; // prevent gc
+        const int WH_KEYBOARD_LL = 13;
+    }
+}
---
+++ KeyBoardTestUtility/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
+
+namespace KeyBoardTestUtility
+{
+    public sealed partial class MainWindow : Window
+    {
+        private static MainWindow m_MainWindow = null;
+        public static MainWindow GetInstance()
+        {
+            if (m_MainWindow == null)
+            {
+                m_MainWindow = new MainWindow();
+            }
+
+            return m_MainWindow;
+        }
+        KeyboardHock keyboardHock;
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            keyboardHock = new KeyboardHock();
+            this.Activated += MainWindow_Activated;
+        }
+
+        private void MainWindow_Activated(object sender, Microsoft.UI.Xaml.WindowActivatedEventArgs args)
+        {
+            if (args.WindowActivationState == WindowActivationState.Deactivated)//窗口后台
+            {
+                // do stuff
+                System.Console.WriteLine();
+            }
+            else
+            {
+                System.Console.WriteLine();
+                // do different stuff
+            }
+        }
+
+        public string a { get; set; } = "请按键！";
+        public void KeyDown(string vkCode)
+        {
+            System.Diagnostics.Debug.WriteLine("按下了" + vkCode.ToString());
+            //var btn = FindChild<Button>(Main, vkCode.ToString());
+            //if (btn != null)
+            //    btn.Background = new SolidColorBrush(Color.FromArgb(255, 123, 210, 111));
+        }
+        public void KeyUp(int vkCode)
+        {
+            //var btn = FindChild<Button>(Main, vkCode.ToString());
+            //if (btn != null)
+            //    btn.Background = (new Button()).Background;
+        }
+        /// <summary>
+        /// 搜索指定名称的子元素
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public T FindChild<T>(DependencyObject obj, string name) where T : FrameworkElement
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is T && (child as T).Name.Equals(name))
+                    return (T)child;
+                else
+                {
+                    T childOfChild = FindChild<T>(child, name);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+            return null;
+        }
+    }
+}
